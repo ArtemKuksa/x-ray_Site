2c80d92bcbefa6e4c10f7c78841e716793911e18
ArtemKuksa <108671209+ArtemKuksa@users.noreply.github.com>
2022-07-25 09:25:30 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -2,32 +2,35 @@ $('.atlas').mouseenter(function(){
     
     $('.submenu_bases').css({"display": "none"});  
     $('.submenu_atlas').css({"display": "block"});
+    $('.rectangle_atlas').css({"transform": "rotate(90deg)"});
 });
 $('.submenu_atlas').mouseleave(function(){   
     $('.submenu_atlas').css({"display": "none"});
+    $('.rectangle_atlas').css({"transform": "rotate(360deg)"});
 });
 
 
 $('.bases').mouseenter(function(){ 
     $('.submenu_atlas').css({"display": "none"});
     $('.submenu_education').css({"display": "none"});
+    $('.rectangle_dataBase').css({"transform": "rotate(90deg)"});
     $('.submenu_bases').css({"display": "block"});
 });
 $('.submenu_bases').mouseleave(function(){   
     $('.submenu_bases').css({"display": "none"});
+    $('.rectangle_dataBase').css({"transform": "rotate(360deg)"});
 });
- $('.img_logo').mouseenter(function(){
-    $('.submenu_bases').css({"display": "none"});
-    $('.submenu_education').css({"display": "none"});
-})
+
 
 $('.education').mouseenter(function(){ 
     $('.submenu_atlas').css({"display": "none"});
     $('.submenu_bases').css({"display": "none"});
+    $('.rectangle_education').css({"transform": "rotate(90deg)"});
     $('.submenu_education').css({"display": "block"});
 });
 $('.submenu_education').mouseleave(function(){   
     $('.submenu_education').css({"display": "none"});
+    $('.rectangle_education').css({"transform": "rotate(360deg)"});
 });
 $('.burger_link').click(function(){
     alert(111);
